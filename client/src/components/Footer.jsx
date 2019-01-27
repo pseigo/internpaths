7588d1a3ea0a20e5f20718ecc014fc34a3dff55b
Sergey Karavaev <sergeykaravaev44@gmail.com>
2019-01-27 00:31:49 -0800

    Add Footer.jsx and footer.css

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Layout} from 'antd';
+import '../assets/styles/footer.css';
+
+const { Footer } = Layout;
+const copyRight = 'Created by Goodfellas nwhacks team 2019';
+
+class FooterBar extends Component {
+    render() {
+        return (
+            <Layout className="layout">
+                <Footer className="footer">
+                    <p className="copyright">&copy; {copyRight}</p>
+                </Footer>
+            </Layout>
+        );
+    }
+}
+
+export default FooterBar;
