09952a6deeed9eef6891f0504a88a42c0f4c8406
Jason Yue <kymotsujason@live.com>
2019-01-27 08:50:26 -0800

    Add easy apply feature
    - Add easy apply feature for compatible forms

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { withGoogleMap, GoogleMap, withScriptjs, Marker, InfoWindow } from 'react-google-maps';
 import { compose } from "recompose";
-import { Drawer } from 'antd';
+import { Popover, Drawer, notification, Button } from 'antd';
+
 
 const GoogleMapReact = compose(
   withScriptjs,withGoogleMap)(props => {
@@ -34,8 +35,33 @@ const GoogleMapReact = compose(
                   <p>{marker.email}</p>
                   <p>{marker.phone}</p>
                   <p>{marker.job_title}</p>
+                  {marker.company_name === "ICA Projects Inc" && !props.success?
+                  <Popover
+                    content="Compatible form found! Would you like to easy-apply?"
+                    title="Easy apply"
+                    trigger="hover"
+                    visible={props.hovered}
+                    onVisibleChange={props.handleHoverChange}
+                  >
+                  <Popover
+                    content={(<a onClick={props.hide}>Close</a>)}
+                    title="Successfully applied!"
+                    trigger="click"
+                    visible={props.clicked}
+                    onVisibleChange={props.handleClickChange}
+                  >
+                    <Button>Easy apply</Button>
+                  </Popover>
+                </Popover>
+                  :
+                    null
+                  }
                   <p>{marker.listing_url}</p>
-                  <p>{marker.stage}</p>
+                  {props.success ?
+                    <p>Interview</p>
+                    :
+                    <p>{marker.stage}</p>
+                  }
               </div>
               </InfoWindow>}
             
@@ -51,7 +77,38 @@ class Maps extends Component {
     super(props);
     this.state = {
       selectedMarker: false,
+      clicked: false,
+      hovered: false,
+      success: false,
     }
+  }
+
+  hide = () => {
+    this.setState({
+      clicked: false,
+      hovered: false,
+    });
+  }
+
+  handleHoverChange = (visible) => {
+    this.setState({
+      hovered: visible,
+      clicked: false,
+    });
+  }
+
+  handleClickChange = (visible) => {
+    this.setState({
+      clicked: visible,
+      hovered: false,
+      success: true, 
+    });
+    const args = {
+      message: 'Success!',
+      description: 'Successfully applied to the job. You will receive an email conformation soon!',
+      duration: 1.5,
+    };
+    notification.open(args);
   }
 
   onMarkerOver = (marker, e) => {
@@ -72,9 +129,17 @@ class Maps extends Component {
       // Important! Always set the container height explicitly
       <div style={{ height: window.innerHeight-138, width: '100%' }}>
         <GoogleMapReact
+          hide={this.hide}
+          handleHoverChange={this.handleHoverChange}
+          handleClickChange={this.handleClickChange}
           selectedMarker={this.state.selectedMarker}
           markers={this.props.markers}
           onMapClick={this.onMapOver}
+          hovered={this.state.hovered}
+          clicked={this.state.clicked}
+          visible={this.state.visible}
+          success={this.state.success}
+          handleVisibleChange={this.handleVisibleChange}
           onMarkerClick={this.onMarkerOver}
           googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAzlL-ahnHzJ5HG4MD8IoC1y2kETuhvajA&v=3.exp&libraries=geometry,drawing,places"
           loadingElement={<div style={{ height: `100%` }} />}
